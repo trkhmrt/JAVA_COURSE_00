9ab041e2348a5fa590244c9ac97a341ea63c32ee
Tarık Hamarat <trkhamarat@gmail.com>
2026-02-03 22:13:46 +0300

    Operators

--- 01_Operators/src/Main.java
+++ 01_Operators/src/Main.java
@@ -18,15 +18,20 @@ public class Main {
 
         // +(Toplama)
 
-        int a = 10;
-        int b = 20;
-        int toplam = a + b;
-        int fark = a - b;
-        int carpim = a * b;
+//        int a = 10;
+//        int b = 20;
+//        int toplam = a + b;
+//        int fark = a - b;
+//        int carpim = a * b;
+//        int bolum = b / a;
+//        int kalan = b % a;
+//
+//        System.out.println("Toplam = " + toplam);
+//        System.out.println("Fark = " + fark);
+//        System.out.println("Çarpım = " + carpim + "dür");
+//        System.out.println("Bölüm = " + bolum);
+//        System.out.println("Bölüm = " + kalan);
 
-        System.out.println("Toplam = " + toplam);
-        System.out.println("Fark = " + fark);
-        System.out.println("Çarpım = " + carpim + "dür");
 
         //+ işareti int-double(matematiksel tipler için toplama yapar.) String veriler için metinleri birleştirme görevi görür
         /* !!!!!!!!!!
@@ -36,6 +41,71 @@ public class Main {
         System.out.println(isim_soyisim);
 
          */
+
+
+        /* KARŞILAŞTIRMA OPERATÖRLERİ*/
+//        int c = 35;
+//        int d = 35;
+//        boolean esittir = c==d;
+//        boolean esit_degildir = c!=d;
+//        boolean buyuktur = c>d;
+//        boolean kucuktur = c<d;
+//        boolean buyuk_veya_esittir = c>=d;
+//        boolean kucuk_veya_esittir = c<=d;
+//
+//
+//        System.out.println(esittir);
+//        System.out.println(esit_degildir);
+//        System.out.println(buyuktur);
+//        System.out.println(kucuktur);
+//        System.out.println(buyuk_veya_esittir);
+//        System.out.println(kucuk_veya_esittir);
+
+        //STRING KIYASLAMA
+//        String isim = "TarıK";
+//        String isim2 = "Tarık";
+//
+//        System.out.println(isim!=isim2);
+
+
+        //* 1- MANTIKSAL OPERATORLER
+         //     &&(ve) ||(veya)
+
+        /*      1.Deger   &   2.Deger       SONUC
+        *          0             1            0
+        *          0             0            0
+        *          1             0            0
+        *          1             1            1
+        *
+        *           Kullanıcı adı &  Şifre    Sonuç
+        *                true         true    giriş yapıldı
+        *                false        true    giriş yapamaz
+        *                false        false   giriş yapamaz
+        *                true         false   giriş yapamaz
+        *
+        *
+        *
+        *
+        *     ||(VEYA BAĞLACI) (toplama yapılır)
+        *
+        *
+        *         /*      (1.Kullanıcı adı  ||   2.eposta )0  && Şifre      SONUC
+        *                            0              1             1         Giriş yapar
+        *                            1              0             1         Giriş Yapar
+        *                            0              0             1         Giriş yapamazsın
+                                     1              0             0         Giriş Yapaamazsın
+         */
+
+
+
+
+
+
+
+
+
+
+
 
     }
 }
