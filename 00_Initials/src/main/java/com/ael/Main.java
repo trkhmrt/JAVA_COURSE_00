f3eb0124fbc0e6af4c638523e8cd64ca9d77b238
Tarık Hamarat <trkhamarat@gmail.com>
2026-02-03 21:10:26 +0300

    01_Operators

--- 00_Initials/src/main/java/com/ael/Main.java
+++ 00_Initials/src/main/java/com/ael/Main.java
@@ -61,6 +61,9 @@ public class Main {
         checkIsTrue0 = false;
         boolean checkIsTrue1 = false;
 
+
+
+
     }
 }
 
---
+++ 01_Operators/src/Main.java
@@ -0,0 +1,41 @@
+public class Main {
+    public static void main(String[] args) {
+        /* OPERATORS */
+        /*
+        * 1- MANTIKSAL OPERATORLER
+        * &&(ve) ||(veya)
+        *
+        * 2-Aritmetik Operatörler()
+        *  + - * / %
+        *
+        * 3-Karşılaştırma Operatörleri
+        *
+        *  == (Eşittir)  !=(Eşit değildir) >(büyük) <(Küçük) >=(büyük veya eşit) <=(küçük veya eşit)
+        *
+        * */
+
+        /*  ARİTMETİK OPERATÖRLER   */
+
+        // +(Toplama)
+
+        int a = 10;
+        int b = 20;
+        int toplam = a + b;
+        int fark = a - b;
+        int carpim = a * b;
+
+        System.out.println("Toplam = " + toplam);
+        System.out.println("Fark = " + fark);
+        System.out.println("Çarpım = " + carpim + "dür");
+
+        //+ işareti int-double(matematiksel tipler için toplama yapar.) String veriler için metinleri birleştirme görevi görür
+        /* !!!!!!!!!!
+        String isim = "Tarık";
+        String soyisim = "Hamarat";
+        String isim_soyisim = isim + " " + soyisim;
+        System.out.println(isim_soyisim);
+
+         */
+
+    }
+}
