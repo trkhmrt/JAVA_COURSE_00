5d753a5f197f667c5347740a33258014fb934f12
Tarık Hamarat <trkhamarat@gmail.com>
2026-02-04 21:33:46 +0300

    Conditionals

---
+++ 02_Conditional/src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+    public static void main(String[] args) {
+        /* IF- ELSE IF - ELSE*/
+
+        /*
+        *  karar/koşul yapıları uygulama içindeki değişkenlerin durumuna göre uygulamanın akışına yön verdirebildiğimiz yapılardır.
+        *
+        * if sadece 1 kere tanımlanır.Alternatif durumların her biri else if ile kontrol edilir.kapsam dışında kalan bütün durumlar için else kullanılır.
+        *
+        *      */
+
+        int yas = 17;
+
+        if(yas>=18){
+            System.out.println("kişi ehliyet alabilir.");
+        }
+        else if(yas==17){
+            System.out.println("1 sene sonra ehliyet alabileceksiniz.üzülmeyin :)");
+        }
+        else{
+            System.out.println("kişi ehliyet alamaz.");
+        }
+
+
+        /* String olarak bir rol bilgisi tanımlancak(Statik)
+        *
+        * girilen rol bilgisi user ise (Sınırlı erişim mesajı yazsın)
+        *
+        * girilen rol admin ise (tam yetki yazsın)
+        *
+        * girilen rol dbmaster ise (tam veri tabanı yetkisi)
+        *
+        * geri kalan bütün durumlar için yetkiniz yok mesajı konsolda yazdırılsın.
+        *  */
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
