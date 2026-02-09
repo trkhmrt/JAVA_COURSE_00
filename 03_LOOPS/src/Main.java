b0fb29c7c0b07a9d22af3557987155584e176444
Tarık Hamarat <trkhamarat@gmail.com>
2026-02-09 22:37:22 +0300

    04_Arrays

--- 03_LOOPS/src/Main.java
+++ 03_LOOPS/src/Main.java
@@ -1,21 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         /*
-        * DÖNGÜLER
-        *
-        *Tekrar etmesi gereken işlemleri döngüler yardımıyla çalıştırabiliyoruz
-        *
-        * DÖNGÜ TÜRLERİ
-        *
-        * 1-FOR
-        * 2-WHILE
-        * 3-DO-WHILE
-        * 3-FOREACH
-        *
-        * ANAHTAR KELİMELER
-        * BREAK-CONTINUE
-        *
-        * */
+         * DÖNGÜLER
+         *
+         *Tekrar etmesi gereken işlemleri döngüler yardımıyla çalıştırabiliyoruz
+         *
+         * DÖNGÜ TÜRLERİ
+         *
+         * 1-FOR
+         * 2-WHILE
+         * 3-DO-WHILE
+         * 3-FOREACH
+         *
+         * ANAHTAR KELİMELER
+         * BREAK-CONTINUE
+         *
+         * */
 
         //i++ demek i yi 1 arttır demek.(1'e özeldir)
         //i+=2 ... i yi istenilen değerde arttırmak için kullanılır
@@ -23,14 +25,57 @@ public class Main {
 //            System.out.println("Hello World");
 //        }
 
-        for (int i = 0; i < 10; i+=2) { //10
-            System.out.println(i); //8
-        }
+//        for (int i = 0; i < 10; i += 2) { //10
+//            System.out.println(i); //8
+//        }
         /*
-        * ilk değer = ? 0
-        * ikinci değer = ? 2
-        * son değer = ? 8
-        * */
+         * ilk değer = ? 0
+         * ikinci değer = ? 2
+         * son değer = ? 8
+         * */
+
+        //==============WHILE===============
+
+//        int sayi = 4;
+//        while (sayi<10) {
+//            System.out.println(sayi);
+//            sayi++;
+//        }
+
+        //Scanner input = new Scanner(System.in);
+//
+//        1.ADIM(Konunun pekiştirilmesi için örnek verildi)
+//        boolean sonuc = !(10>9);
+//        System.out.println(sonuc);
+//
+//        2.ADIM(Konunun pekiştirilmesi için örnek verildi)
+//        String isim = "tarık";
+//        boolean isim_kontrol = !isim.equals("tarık");
+//        System.out.println(isim_kontrol);
+
+//        String isim = "";
+//        while(!isim.equals("tarık")) {
+//            System.out.println("Giriniz: ");
+//            isim = input.nextLine();
+//        }
+
+
+        //DO-WHILE
+
+
+        //Foreach
+//        String alfabe = "abcdefg";
+//
+//        for(char karakter:alfabe.toCharArray()){
+//            System.out.println(karakter);
+//        }
+
+
+
+
+
+
+
 
 
 
---
+++ 04_Arrays/src/Main.java
@@ -0,0 +1,117 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * ARRAYS(Diziler
+        *
+        *Aynı veri tipine sahip birden fazla değişkeni tek bir değişken altında toplanmasını sağlar
+        *
+        *
+        *  veri_tipi[] dizi_ismi = new veri_tipi[eleman_sayısı];
+        * */
+
+        //Dizi tanımlama 1.Yöntem
+//        int[] sayilar = new int[3];
+//        sayilar[0] = 10;
+//        sayilar[1] = 20;
+//        sayilar[2] = 30;
+//
+//        String[] isimler = new String[6];
+//        isimler[0] = "Ayşe";
+//        isimler[1] = "Buse";
+//        isimler[2] = "Tarık";
+//        isimler[3] = "Elanur";
+//        isimler[4] = "Leal";
+//        isimler[5] = "Furkan";
+
+        //2.Dizi Tanımlama Yöntemi
+
+//        double[] notlar = {65.42,78.12,99.13,23.19};
+//        System.out.println(notlar.length);
+
+        //Dizi elamanlarına erişme
+//
+//        for (int i = 0; i < notlar.length; i++) {
+//            System.out.println(notlar[i]);
+//        }
+
+        //Foreach YÖNTEMİ
+//        for(double item:notlar){
+//            System.out.println(item);
+//        }
+
+        //ARRAY METOTLARI
+
+//        String[] isimler = new String[6];
+//        isimler[0] = "Ayşe";
+//        isimler[1] = "Buse";
+//        isimler[2] = "Tarık";
+//        isimler[3] = "Elanur";
+//        isimler[4] = "Leal";
+//        isimler[5] = "Furkan";
+
+//        isimler[0] = "zeynep";
+//        System.out.println(isimler[0]);
+
+        //SORT(A-z 1-9 sıralaması yapar)
+//        Arrays.sort(isimler);
+//
+//
+//        System.out.println(Arrays.toString(isimler));
+
+        //BinarySearch (Aranan değerin indeksini döndürür)
+
+//        int indeks = Arrays.binarySearch(isimler,"Furkan");
+//
+//        System.out.println(indeks);
+
+        //COPYOF
+
+        //İstanbul
+        //Adres Dğeri Gültepe
+        int[] sayilar = {20,30,90,14};
+
+        System.out.println("Sayilar Dizisi:"+Arrays.toString(sayilar));
+
+        //Kağıthane
+        int[] sayilar2 = {10,30,20,30};
+        int refNO = System.identityHashCode(sayilar);
+        System.out.println(refNO);
+        sayilar2 = sayilar;
+
+        System.out.println("Sayilar2 Dizisi:"+Arrays.toString(sayilar2));
+
+        sayilar2[0] = 39;
+        int refNO2 = System.identityHashCode(sayilar2);
+        System.out.println(refNO2);
+
+        System.out.println("Sayilar Dizisi:"+Arrays.toString(sayilar));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
